ce5d5f285f04fdf150b09a4362f5a050af5a7408
DESKTOP-H6N45NK\User <mhonarvar18@gmail.com>
2022-08-03 11:35:40 +0430

    crud operaions

--- src/feed/models/post.entity.ts
+++ src/feed/models/post.entity.ts
@@ -1,13 +1,14 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+/* eslint-disable prettier/prettier */
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('feed_post')
 export class FeedPostEntity {
     @PrimaryGeneratedColumn()
     id: number;
 
-    @Column({default: ''})
+    @Column({ default: '' })
     body: string;
 
-    @Column()
+    @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
     createdAt: Date;
 }
